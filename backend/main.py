3353a6d03fde644e8355a3a162a33fd5043fc074
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-10-31 16:08:33 +0900

    add POST states mock

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette import status
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class PostStatesReq(BaseModel):
+    device_id: str
+    weight: int
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/states", status_code=status.HTTP_204_NO_CONTENT)
+def post_states(req: PostStatesReq):
+    print(req)
+    return {}
