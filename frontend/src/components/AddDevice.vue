d115e79f6132d874ffbbfce6f14ec4a4ae94524a
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-31 17:06:37 +0900

    追加ボタン

---
+++ frontend/src/components/AddDevice.vue
@@ -0,0 +1,28 @@
+<template>
+    <section id="AddDevice">
+        <AddButton />
+        <AddDialog />
+    </section>
+</template>
+
+<script>
+import AddButton from './molecutes/AddButton'
+import AddDialog from './molecutes/AddDialog'
+
+export default {
+    name: 'AddDevice',
+    components: {
+        AddButton,
+        AddDialog
+    }
+}
+</script>
+
+<style scoped>
+#AddDevice {
+    width: 200px;
+    height: 300px;
+    border: 2px dotted #111;
+    border-radius: 30px;
+}
+</style>
---
+++ frontend/src/components/molecutes/AddButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <span class="add">+</span>
+        <h1>追加する</h1>
+    </section>
+</template>
+
+<style scoped>
+.add{
+    margin-top:30px;
+    font-size:80px
+}
+</style>
