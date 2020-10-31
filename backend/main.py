96d96ffc77bb304d98296daaf77ff122194c45ab
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-10-31 16:15:47 +0900

    add GET /states mock

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette import status
@@ -14,3 +16,33 @@ class PostStatesReq(BaseModel):
 def post_states(req: PostStatesReq):
     print(req)
     return {}
+
+
+class GetStatesResState(BaseModel):
+    device_id: str
+    item: str
+    weight: int
+    percentage: int
+
+
+class GetStatesRes(BaseModel):
+    states: List[GetStatesResState]
+
+
+@app.get("/states", response_model=GetStatesRes)
+def get_states():
+    # TODO: replace with real data
+    return {"states": [
+        {
+            "device_id": "5CDCD567",
+            "item": "牛乳",
+            "weight": 500,
+            "percentage": 52
+        },
+        {
+            "device_id": "5CDCD568",
+            "item": "コーヒー牛乳",
+            "weight": 500,
+            "percentage": 52
+        }
+    ]}
