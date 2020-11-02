5cea10d870df640fb5f7369d74949cc01f7cf6ae
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-11-02 12:54:32 +0900

    こっちも整形やん

--- frontend/src/components/AddDevice.vue
+++ frontend/src/components/AddDevice.vue
@@ -1,43 +1,43 @@
 <template>
-    <section>
-        <AddDialog v-if="isOpened"/>
-        <section id="AddDevice">
-            <AddButton @open-modal="openModal" />
-        </section>
+  <section>
+    <AddDialog v-if="isOpened" />
+    <section id="AddDevice">
+      <AddButton @open-modal="openModal" />
     </section>
+  </section>
 </template>
 
 <script>
-import AddButton from './molecutes/AddButton'
-import AddDialog from './molecutes/AddDialog'
+import AddButton from './molecutes/AddButton';
+import AddDialog from './molecutes/AddDialog';
 
 export default {
-    name: 'AddDevice',
-    components: {
-        AddButton,
-        AddDialog
-    },
+  name: 'AddDevice',
+  components: {
+    AddButton,
+    AddDialog,
+  },
 
-    data () {
-        return {
-            isOpened: false
-        }
-    },
+  data() {
+    return {
+      isOpened: false,
+    };
+  },
 
-    methods: {
-        openModal () {
-            this.isOpened= true
-            console.log(this.isOpened)
-        }
-    }
-}
+  methods: {
+    openModal() {
+      this.isOpened = true;
+      console.log(this.isOpened);
+    },
+  },
+};
 </script>
 
 <style scoped>
 #AddDevice {
-    width: 200px;
-    height: 300px;
-    border: 2px dotted #111;
-    border-radius: 30px;
+  width: 200px;
+  height: 300px;
+  border: 2px dotted #111;
+  border-radius: 30px;
 }
 </style>
