e47ee5b5f9ad75539d9065f5b439ffc3bd0c79a6
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-11-02 12:29:56 +0900

    device追加機能とりあえずとちゅう

---
+++ frontend/src/components/molecutes/AddDialog.vue
@@ -0,0 +1,26 @@
+<template>
+    <dialog open id="Dialog">
+        <h1>デバイスを追加</h1>
+        <form>
+            <input type="text" label="上に置くものの名前" />
+            <input />
+            <input />
+            <input />
+            <input />
+        </form>
+    </dialog>    
+</template>
+
+<script>
+export default {
+    name: 'AddDialog'
+}
+</script>
+
+<style scoped>
+#Dialog{
+    width:80%;
+    height: 300px;
+    border: 1px solid #111
+}
+</style>
