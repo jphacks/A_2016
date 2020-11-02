2efc0c974779dfa640adb9e9e64c2c865cb458b6
gojiteji <mail@gojiteji.com>
2020-11-02 21:29:29 +0900

    reset&items

--- backend/main.py
+++ backend/main.py
@@ -12,11 +12,12 @@ dbname="d4v2bqr9sa4l5v"
 user="rwuiymiymxajli"
 password="f37f88c526282ce854d6356b27bc0ae75fd92e23bef0a83f76b81974d7299a26"
 conn=psycopg2.connect("host="+server+" port="+port+" dbname="+dbname+" user="+user+" password="+password)
+cursor = conn.cursor()
 try:
     cursor.execute('CREATE TABLE devices (id char(16),name char(16),max real,min real,cur real, expdate real)')
 except:
     print("DB already exists")
-cursor = conn.cursor()
+
 
 app = FastAPI()
 class PostStatesReq(BaseModel):
@@ -66,10 +67,25 @@ class PostItemsReq(BaseModel):
     max: int
     min: int
     expdate: int#unix time
-
-
+    
 @app.post("/items", status_code=status.HTTP_201_CREATED)
 def post_items(req: PostItemsReq):
     print(req)
+    try:
+        cursor.execute('INSERT INTO devices (id ,name,max,min,expdate) VALUES(%s, %s, %s, %s, %s)',(req.device_id,req.item,req.max,req.min, req.expdate,))
+        conn.commit()
+        return {"text": "Created"}
+    except:
+        return {"text": "error"}
 
-    return {}
+@app.get("/reset", status_code=status.HTTP_201_CREATED)
+def post_items():
+    try:
+        cursor.execute('DELETE FROM devices')
+        conn.commit()
+    except Exception as e:
+        print("DB error",e)
+        conn.rollback()
+        pass
+    return {"text": "reseted"}
+ 
