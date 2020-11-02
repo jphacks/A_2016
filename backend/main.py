faf3c887c5c5ba1b013dc471ac057fa1a5acd79b
gojiteji <mail@gojiteji.com>
2020-11-02 19:29:26 +0900

    db connection added

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,22 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette import status
+import pandas as pd
+import psycopg2
+
+server="ec2-3-208-224-152.compute-1.amazonaws.com"
+port="5432"
+dbname="d4v2bqr9sa4l5v"
+user="rwuiymiymxajli"
+password="f37f88c526282ce854d6356b27bc0ae75fd92e23bef0a83f76b81974d7299a26"
+conn=psycopg2.connect("host="+server+" port="+port+" dbname="+dbname+" user="+user+" password="+password)
+try:
+    cursor.execute('CREATE TABLE devices (id char(16),name char(16),max real,min real,cur real, expdate real)')
+except:
+    print("DB already exists")
+cursor = conn.cursor()
 
 app = FastAPI()
-
-
 class PostStatesReq(BaseModel):
     device_id: str
     weight: int
@@ -47,15 +59,17 @@ def get_states():
         }
     ]}
 
-
+#Register item
 class PostItemsReq(BaseModel):
     device_id: str
     item: str
     max: int
     min: int
+    expdate: int#unix time
 
 
 @app.post("/items", status_code=status.HTTP_201_CREATED)
 def post_items(req: PostItemsReq):
     print(req)
+
     return {}
