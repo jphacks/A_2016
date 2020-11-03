71f23759c7d4759e1b4470365dafe07aa278537b
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-03 21:15:39 +0900

    db upgrade

--- backend/app/domain/entity/devices.py
+++ backend/app/domain/entity/devices.py
@@ -5,8 +5,8 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 class Device(Base):
     __tablename__ = "devices"
 
-    id = Column(String, primary_key=True)
-    item = Column(String)
+    id = Column(String(length=32), primary_key=True)
+    item = Column(String(length=255))
     max = Column(Integer)
     min = Column(Integer)
     weight = Column(Integer)
