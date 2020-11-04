0bed8accfed22c85dad398b69b881b4a94dc31d6
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-04 14:00:01 +0900

    impl update device func

--- backend/app/domain/schemas/devices.py
+++ backend/app/domain/schemas/devices.py
@@ -5,16 +5,6 @@ class DeviceBase(BaseModel):
     id: str
 
 
-class DeviceCreate(DeviceBase):
-    item: str
-    max: int
-    min: int
-
-
-class DeviceStateUpdate(DeviceBase):
-    weight: int
-
-
 class Device(DeviceBase):
     item: str
     max: int
