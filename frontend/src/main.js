5e921fbb7dc52d20a5631cc24f0d5f94ef0e6d9b
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-05 22:13:03 +0900

    install vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
 new Vue({
-    render: h => h(App),
-  }).$mount('#app')
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
