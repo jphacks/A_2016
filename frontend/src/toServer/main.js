b5f52ad8084748ba9e71e3d2fb895e663fd8d902
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-11-05 12:15:02 +0900

    コメントアウト

--- frontend/src/toServer/main.js
+++ frontend/src/toServer/main.js
@@ -5,6 +5,7 @@ const serverURL = 'https://a-2016-backend.herokuapp.com';
 // 全てのデバイスの％などをget
 export const hello = () => {
   Axios.get(`${serverURL}/devices`).then((res) => {
+    console.log('connect');
     return res.data;
   });
 };
@@ -12,6 +13,7 @@ export const hello = () => {
 // deviceの登録（変更もここで）
 export const register = (item) => {
   Axios.post(`${serverURL}/devices`, item).then((res) => {
+    console.log('connect');
     return res.data;
   });
 };
