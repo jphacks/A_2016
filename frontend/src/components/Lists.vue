56730b015bc70400c11bd5b3826674b214173d0d
tanakarn98 <56716847+tanakarn98@users.noreply.github.com>
2020-11-06 22:52:35 +0900

    文字色がいい感じになるやつ

--- frontend/src/components/Lists.vue
+++ frontend/src/components/Lists.vue
@@ -16,7 +16,7 @@
           id="svg-bg"
           class="wave"
           :style="`top:${
-            list && list.percentage == 0
+            list && list.percentage <= 0
               ? 170
               : 150 - (list && list.percentage * 1.5)
           }px; `"
@@ -124,7 +124,7 @@ export default {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   .listCard {
-    background-color: whitesmoke;
+    background-color: white;
     margin: 10px auto;
     width: 80%;
     max-width: 150px;
@@ -134,15 +134,20 @@ export default {
     border: 1px solid #c3c3c3;
     cursor: pointer;
     position: relative;
+    // mix-blend-mode: hue;
     .percentage {
       position: absolute;
       top: 5px;
       left: 10px;
+      mix-blend-mode: difference;
     }
     .name {
+      // color: rgb(59, 59, 59);
       position: absolute;
       bottom: 5px;
       left: 10px;
+      mix-blend-mode: difference;
+      // mix-blend-mode: lighten;
     }
   }
 }
@@ -164,6 +169,7 @@ export default {
   animation: wave 5s infinite normal linear;
   width: 200%;
   height: 100%;
+  // mix-blend-mode: hue;
 }
 @keyframes wave {
   0% {
