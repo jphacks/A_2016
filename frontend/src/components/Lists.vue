16c22b5b223742a22d67e948451912104bacac3e
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-11-07 11:31:38 +0900

    change font size and more

--- frontend/src/components/Lists.vue
+++ frontend/src/components/Lists.vue
@@ -185,9 +185,9 @@ export default {
   transform: translateY(calc(-50% - 0px)) scale(1, -1);
   left: 0;
   overflow-y: hidden;
-  animation: wave 5s infinite normal linear;
-  width: 200%;
-  height: 100%;
+  animation: wave 4s infinite normal linear;
+  width: 280%;
+  height: 40%;
   // mix-blend-mode: hue;
 }
 @keyframes wave {
@@ -195,7 +195,7 @@ export default {
     left: 0%;
   }
   100% {
-    left: -80%;
+    left: -110%;
   }
 }
 
