feed202f373725d8abbfa6c05c3f7fed7ab1b59d
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-16 15:52:50 +0900

    add mock data

--- backend/app/router/v2/router.py
+++ backend/app/router/v2/router.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from app.db import Database
@@ -7,8 +10,33 @@ from app.db import Database
 def new_router(db: Database):
     router = APIRouter()
 
+    class GetContainersResContainer(BaseModel):
+        id: str
+        image: str
+        name: str
+        max: int
+        min: int
+
+    class GetContainersRes(BaseModel):
+        containers: List[GetContainersResContainer]
+
     @router.get("/containers")
     def get_containers(ssn: Session = Depends(db.get_db)):
-        return {"msg": 'hello'}
+        return GetContainersRes(containers=[
+            GetContainersResContainer(
+                id="C51CD0F5",
+                image="https://i.picsum.photos/id/202/200/200.jpg",
+                name="牛乳パック",
+                max=1040,
+                min=40
+            ),
+            GetContainersResContainer(
+                id="C51CD0F6",
+                image="https://i.picsum.photos/id/202/200/200.jpg",
+                name="500mlペットボトル",
+                max=540,
+                min=40
+            )
+        ])
 
     return router
