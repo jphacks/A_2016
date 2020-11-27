28697052b65b63d748c152ce2eb983c5f7694a91
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-28 07:35:54 +0900

    qrで登録できるように

--- frontend/src/components/Lists.vue
+++ frontend/src/components/Lists.vue
@@ -99,7 +99,7 @@ export default {
       loading: true,
       isOpenedAdd: false,
       deviceIdFromURL: '',
-      reset: false
+      reset: false,
     };
   },
 
@@ -131,10 +131,18 @@ export default {
     },
   },
 
+  async mounted() {
+    this.query = new URLSearchParams(location.search.slice(1));
+    this.deviceIdFromURL = this.query?.get('d');
+    if (this.deviceIdFromURL) {
+      this.isOpenedAdd = true;
+    }
+  },
+
   methods: {
     openAddModal() {
       this.isOpenedAdd = true;
-      this.reset = !this.reset
+      this.reset = !this.reset;
     },
 
     async closeAddModal() {
