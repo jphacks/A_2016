8553b08b512d9b2b5101e10f2466e158c606d115
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-27 16:29:24 +0900

    add Login.vue

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="#firebaseui-auth-container"></div>
+</template>
+
+<script>
+import { ui } from '../firebase/ui';
+import firebase from 'firebase/app';
+
+export default {
+  name: 'Login',
+  created() {
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        {
+          provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          requireDisplayName: false,
+        },
+      ],
+    });
+  },
+};
+</script>
+
+<style></style>
