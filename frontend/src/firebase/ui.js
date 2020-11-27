49470394d366cf5ce0ef4a978038ad5bd0cb2d72
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-11-27 16:19:08 +0900

    init firebase

---
+++ frontend/src/firebase/ui.js
@@ -0,0 +1,3 @@
+import 'firebaseui';
+
+export const ui = new firebaseui.auth.AuthUI(firebase.auth());
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import { devicesStore } from './store/devices';
 
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZgk8B4u1xRnr1GhdO7UNboKgk03bStEo",
+  authDomain: "arcana-295709.firebaseapp.com",
+  databaseURL: "https://arcana-295709.firebaseio.com",
+  projectId: "arcana-295709",
+  storageBucket: "arcana-295709.appspot.com",
+  messagingSenderId: "707842401316",
+  appId: "1:707842401316:web:5b151d1237176748a9e4b1",
+  measurementId: "G-76QVRJLQCX"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 new Vue({
   vuetify,
   devicesStore,
