9bb6a91f191b23ec0db1ee788f34463beaa24c09
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-11-27 23:11:12 +0900

    about template

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <v-card class="text">
+            <v-card-subtitle>どこにアルカナ？</v-card-subtitle>
+            <v-card-title><p>arcana</p></v-card-title>
+            <v-card-text>デバイスの残量を可視化できるかんな？</v-card-text>
+        </v-card>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+p{
+    font-size:120%;
+    text-align: center;
+}
+</style>
