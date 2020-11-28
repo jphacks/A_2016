c24ea0059dbecad31cda61fe6f0574be3cbd72f3
tanakarn98 <56716847+tanakarn98@users.noreply.github.com>
2020-11-28 09:32:19 +0900

    ogpとabout

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -1,38 +1,54 @@
 <template>
-    <v-card class="about">
-        <p class="head">残量を可視化できる!<span style="font-size:300%;font-weight:bold">      arcana</span></p>
-        <v-card-title> - arcana について</v-card-title>
-        <p>
-            - このアプリケーションでは、デバイスに載せる商品の詳細を登録することで、その商品の残量を可視化するものです。
-        </p>
-        <v-card-title> - 特徴</v-card-title>
-        <p>
-            - 牛乳など冷蔵庫にて保管するものをはじめ、シャンプーや消毒液などを登録させることができます。
-        </p>
-        <p>
-            - その商品の表示の色や消費期限を登録することができます。
-        </p>
-        <p>
-            - 商品を予め登録しておくことで、残量が少なくなったら「購入する」ボタンが出現し、その商品を購入するウェブサイトに遷移します。
-        </p>
-        </v-card>
+  <v-card class="about">
+    <p class="head">残量を可視化できる!</p>
+    <v-spacer></v-spacer>
+    <img :src="image_src" />
+    <v-spacer></v-spacer>
+    <v-card-title>arcana について</v-card-title>
+    <p>
+      -
+      このアプリケーションでは、デバイスに載せる商品の詳細を登録することで、その商品の残量を可視化するものです。
+    </p>
+    <v-card-title>特徴</v-card-title>
+    <p>
+      -
+      牛乳など冷蔵庫にて保管するものをはじめ、シャンプーや消毒液などを登録させることができます。
+    </p>
+    <p>- その商品の表示の色や消費期限を登録することができます。</p>
+    <p>
+      -
+      商品を予め登録しておくことで、残量が少なくなったら「購入する」ボタンが出現し、その商品を購入するウェブサイトに遷移します。
+    </p>
+  </v-card>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      image_src: require('../../../images/arcana_logo.png'),
+    };
+  },
+};
 </script>
 
-<style scoped>
-.head{
-    text-align: center;
+<style scoped lang="scss">
+.head {
+  text-align: center;
 }
-.about{
-    margin:0;
-    padding-left:10px;
-    padding-right:10px;
-    padding-bottom:10px;
-    text-align: left;
+.about {
+  margin: 0;
+  padding: 20px;
+  text-align: left;
+  img {
+    width: 70%;
+    max-width: 250px;
+    display: block;
+    margin: 20px auto;
+  }
+}
+.v-card__title {
+  font-weight: bold !important;
+  padding: 10px 0 !important;
 }
 </style>
